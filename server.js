4e1291cdb2452a6e731e865a4b0149538029d9f7
Malik-2-Wavy <psgpremier98@gmail.com>
2026-04-25 05:09:22 -0400

    Update server.js

--- server.js
+++ server.js
@@ -9,7 +9,7 @@ const app = express();
 const PORT = process.env.PORT || 3000;
 
 // --- Auto-Update Config ---
-const LATEST_VERSION = "1.1";
+const LATEST_VERSION = "1.2";
 const LOADER_FILENAME = "Phantomwre Loader.exe";
 // Note: Ensure this file exists in the same directory as server.js
 
